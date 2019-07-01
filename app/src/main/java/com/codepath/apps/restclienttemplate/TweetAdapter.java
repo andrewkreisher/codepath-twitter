27dfc69d425e53ba1d33eec96ee89ea89b8e2eac
andrewkreisher <andrewkreisher@fb.com>
2019-07-01 16:16:53 -0700

    Add login functionality

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,74 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
+
+    private List<Tweet> mTweets;
+    //pass in tweet array into constructor
+     public TweetAdapter(List<Tweet> tweets){
+         mTweets = tweets;
+     }
+
+    //inflate layout for each row and cache references into viewholder
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, viewGroup, false );
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+
+    }
+
+    //bind values based on position of element
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        //get data
+        Tweet tweet = mTweets.get(i);
+        //populate view w data
+        viewHolder.tvUsername.setText(tweet.user.name);
+        viewHolder.tvBody.setText(tweet.body);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    //create viewholder class
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            //perform findviewbyid lookups
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUsername);
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,29 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+
+    //attributes
+    public String body;
+    public long uid;
+    public String createdAt;
+    public User user;
+
+    //deserialize data (Json)
+    public static Tweet fromJSON (JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        //extract info from json
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        return tweet;
+
+
+    }
+
+
+}
